3ccb375e888fc4039720e74f36adf49b7f100bf1
Yogendra Baskota <pas077bei045@wrc.edu.np>
2023-12-25 16:04:04 +0545

    .

---
+++ day_012.py
@@ -0,0 +1,19 @@
+name = "sujan,yogendra"
+
+print(name[0:7]) #display name from 0 to 6th poosition
+print(name[:7]) #python automatically count from 0
+print(name[:])  #python automatically display from first to last
+print(name[0:-4]) #python interpret it as 0 to len(name)-4 = 0 to (14-4) = 0 to 10 and it gives sujan,yoge
+print(name[-5:-1])  #len(name)-5:len(name)-1 = 9:13 so it gives endra
+
+
+
+print(len(name)) #count the number of strings
+
+car = "lamborgini"
+len1 = len(car) #count letters in the word(string) stored in car
+print("word lamborgini has ", len1, "letters")
+
+
+
+
