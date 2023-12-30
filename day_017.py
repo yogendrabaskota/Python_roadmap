1acb08936a92ebadf436027d3aead3b777a74513
Yogendra Baskota <pas077bei045@wrc.edu.np>
2023-12-30 21:04:02 +0545

    .

---
+++ day_017.py
@@ -0,0 +1,20 @@
+# Loops in Python
+#name = "Abhishek"
+#for i in name:
+#    print(i)
+#    if(i == "b"):
+#        print("this is something special")
+       
+# for list
+        
+#colors = ["red","green","blue","yellow"]
+#for item in colors:
+#    print(item)
+#    for i in item:
+#        print(i)
+#        print("\n")
+
+#range()
+for k in range(5):
+ print(k,"\n") # 0 to 5
+ 
