b13563f32262c8d94c1e56b091335c4c18b71cf6
Yogendra Baskota <pas077bei045@wrc.edu.np>
2023-12-31 14:34:35 +0545

    .

---
+++ day_018.py
@@ -0,0 +1,14 @@
+# while loop
+count = 5
+while (count > 0):
+  print(count) 
+  count = count - 1
+
+print("\n")
+#else with while
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('counter is 0') # print this when condition of while is not satisfied
