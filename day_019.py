bd315d41071cd52dc1da6b888c1c26be59135f61
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-01 20:51:58 +0545

    .

---
+++ day_019.py
@@ -0,0 +1,15 @@
+# break and continue statement 
+#break
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        print("Mississippi")
+print("Thank you\n")
+
+#continue
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
