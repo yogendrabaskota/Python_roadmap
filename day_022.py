075c1ee2dca2a2457eabef4aea27693de0288953
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-04 20:46:20 +0545

    .

---
+++ day_022.py
@@ -0,0 +1,25 @@
+# List in python
+
+l = [1,2,3]
+print(l)
+print(type(l)) # type of l i.e list
+
+marks = [20,33,19,21]
+print(marks[0]) # print first marks # indexing starts from first and from 0
+print(marks[1]) # print second marks
+print(marks[3]) # print third marks
+print(marks[-3]) # indexing starts from last and from 1
+print(marks[1:3]) # print first but not third i.e 1 & 2
+
+details = ["Abhijeet", 18, "Rajkumar", 9.8]
+print(details)
+
+
+
+
+# List is made when we want to store different data with a single name.
+# the data is only differentiated by its index number. (indexing can be positive e.g 1,2,3 and negative -1,-2)
+# Lists are ordered and separated by , and enclosed in [ ].
+# Lists are mutable.
+# Lists can have multiple data types. string and int in the same list but its not recommended.
+# if can be used to check if a data is available in the list. # List comprehension means generating lists during execution of program. conditions can also be given
