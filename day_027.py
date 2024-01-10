0e4b7b156f2cfee91772ddc90985b789581118ef
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-10 22:48:45 +0545

    .

---
+++ day_027.py
@@ -0,0 +1,5 @@
+# create a program capable of displaying a question to a user like KBC
+# Use list datatype to store question and their correct answer
+# display the final amount the person is taking home after playing the game
+  
+# solution in 39
