7274571fd6e5fc813f07516c2c3a64ea2389e9a6
Yogendra Baskota <pas077bei045@wrc.edu.np>
2024-01-16 20:54:10 +0545

    .

---
+++ day_031.py
@@ -0,0 +1,14 @@
+# Python sets
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+info = {"Carla", 19, False, 5.9}
+for item in info:
+    print(item)
+
+# Sets are unordered collection of data items. 
+# They store multiple items in a single variable.
+# Set items are separated by commas and enclosed within curly brackets {}. 
+# Sets are unchangeable, meaning you cannot change items of the set once created.
+# Sets do not contain duplicate items.
+    
